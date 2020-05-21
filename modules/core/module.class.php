7baca871b2013f4a8e77397662b78106c8467e21
Victor PS <victor.paysaut@gmail.com>
2020-05-22 01:48:13 +0200

    Added Core's module first things

--- modules/core/module.class.php
+++ modules/core/module.class.php
@@ -2,5 +2,19 @@
 
 class CoreModule extends BaseModule
 {
-
+    /**
+     * The only un-timestamped request.
+     * Used as healthstatus to see if the server is OK.
+     * Returns the timestamp of the server, for protecting later requests
+     * against replay attacks.
+     * Also returns the list of available modules.
+     */
+    public function api_healthstatus() {
+        global $MODULES;
+        header("HTTP/1.1 418 I'm a teapot");
+        return jsonback(array(
+            'time' => time(),
+            'modules' => $MODULES
+        ));
+    }
 }
--- modules/core/module.vue.js
+++ modules/core/module.vue.js
@@ -1,3 +1,22 @@
 // core.component
-"use strict";
 
+Vue.component('module-core', {
+    props: ['session', 'module', 'isModule'],
+    data: function() {
+        return {
+            //
+        };
+    },
+    template: `
+        <section style="">
+            Module CORE - welcome page
+        </section>`,
+    computed: {
+        //
+    },
+    methods: {
+        wakeUp: function(firstTime) {
+            //
+        },
+    }
+});
