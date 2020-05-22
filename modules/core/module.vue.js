0fbf2160e1960813a039e9ea6940d4d9a070243a
Victor PS <victor.paysaut@gmail.com>
2020-05-22 22:33:20 +0200

    Notes : front-end part of commit 7af9133

--- modules/core/module.vue.js
+++ modules/core/module.vue.js
@@ -4,19 +4,60 @@ Vue.component('module-core', {
     props: ['session', 'module', 'isModule'],
     data: function() {
         return {
-            //
+            noteContent: "",
+            noteContentInitial: ""
         };
     },
     template: `
-        <section style="">
-            Module CORE - welcome page
+        <section style="color:black;background-image:linear-gradient(to bottom right, #E3FF9D, #FFDE85);">
+            <div class="columns">
+                <div class="column col-6">
+                    <h2>Welcome to this server !</h2>
+                    <dl>
+                        <dt>Server description</dt>
+                        <dd>{{ server.desc }}</dd>
+
+                        <dt>Domain</dt>
+                        <dd>{{ server.domain }}</dd>
+
+                        <dt>Romanishell URL</dt>
+                        <dd>{{ server.url }}</dd>
+
+                        <dt>Secret Key <a @onclick="" title="Renew"><span class="silkicon arrow_refresh"></span></a></dt>
+                        <dd>{{ server.secretkey }}</dd>
+                    </dl>
+                </div>
+                <div class="column col-6">
+                    <h2>Notes</h2>
+                    <textarea class="form-input" rows="10" @blur="saveNote" placeholder="Enter notes" v-model="noteContent"></textarea>
+                </div>
+            </div>
         </section>`,
     computed: {
-        //
+        server: function() {
+            if (this.session in serverList) {
+                return serverList[this.session];
+            }
+            return null;
+        }
     },
     methods: {
         wakeUp: function(firstTime) {
-            //
+            if (firstTime) {
+                app.localAPIcall('getnote', {"servid":this.session}, (d) => {
+                    if ('content' in d) {
+                        this.noteContent = d.content;
+                        this.noteContentInitial = d.content;
+                    }
+                });
+            }
         },
+        saveNote: function() {
+            if (this.noteContent !== this.noteContentInitial) {
+                app.localAPIcall('savenote', {"servid":this.session, "content":this.noteContent}, (d) => {
+                    this.noteContentInitial = this.noteContent;
+                });
+            }
+        }
     }
 });
